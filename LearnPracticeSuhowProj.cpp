f91c398be697460ac0e2af47a1f01b30542c94e8
Egor Holkin <egorkholkin2018@gmail.com>
2025-01-24 18:29:30 +0300

    Add new files.

---
+++ LearnPracticeSuhowProj.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+class User {
+public: 
+    long int phoneNumber;
+    long int money;
+
+public:
+
+};
+
+class Bank{
+public:
+    User* _users;
+
+public:
+
+};
+
+
+
+
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
